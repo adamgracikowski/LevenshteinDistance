5b2f29d05910d4f8a2f77ba9673b73a81fd4597e
Adam Grącikowski <admgrac@gmail.com>
2024-12-14 19:13:13 +0100

    add google test project for unit testing

---
+++ LevenshteinDistance/LevenshteinDistance.Tests/LevenshteinDistanceTest.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include "../LevenshteinDistance/LevenshteinDistance.h"
+
+#include <string>
+#include <vector>
+
+class LevenshteinDistanceTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+
+    }
+
+    void TearDown() override {
+
+    }
+};
+
+TEST_F(LevenshteinDistanceTest, IdenticalWords) {
+    FAIL();
+}
+
+TEST_F(LevenshteinDistanceTest, EmptySource) {
+    FAIL();
+}
+
+TEST_F(LevenshteinDistanceTest, EmptyTarget) {
+    FAIL();
+}
+TEST_F(LevenshteinDistanceTest, SingleSubstitution) {
+    FAIL();
+}
+
+TEST_F(LevenshteinDistanceTest, LargeDistance) {
+    FAIL();
+}
---
+++ LevenshteinDistance/LevenshteinDistance/LevenshteinDistance.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <algorithm>
+
+int LevenshteinDistance(const std::string& sourceWord, const std::string& targetWord, std::string& transformation) {
+    return 0;
+}
